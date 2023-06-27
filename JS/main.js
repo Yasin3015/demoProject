d49dc1469cb9c4746d024039629032e413f1083c
Yasin Marei <59691385+Yasin3015@users.noreply.github.com>
2023-06-27 07:24:05 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+var playButton = document.getElementById("playButton");
+
+playButton.addEventListener("click", function() {
+  window.open("https://www.r4m.co/wp-content/uploads/2021/09/R4_sottotitoli-ENG.mp4?19909", "_blanck");
+});
